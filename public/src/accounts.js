b49c04a1678aaddfeaad155eb9269fd1c0ae69e6
Michael Wegner <wegnermichael14@gmail.com>
2023-03-30 18:01:37 -0500

    filled in methods for classes

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -9,9 +9,40 @@ function sortAccountsByLastName(accounts) {
   );
 }
 
-function getTotalNumberOfBorrows(account, books) {}
+function getTotalNumberOfBorrows(account, books) {
+  let times = 0;
+  const borrowed = books.forEach((book) => {
+    if (!!book.borrows) {
+      book.borrows.forEach((accounts) => {
+        if (accounts.id === account.id) {
+          times++;
+        }
+      });
+    }
+  });
+  return times;
+}
 
-function getBooksPossessedByAccount(account, books, authors) {}
+function getBooksPossessedByAccount(account, books, authors) {
+  const borrowedBooks = [];
+  books.forEach((book) => {
+    let bookBorrows = book.borrows;
+    bookBorrows.forEach((borrow) => {
+      if (borrow.id === account.id && !borrow.returned) {
+        borrowedBooks.push(book);
+      }
+    });
+  });
+  let result = borrowedBooks.map((book) => {
+    return { ...book, author: getAuthor(book, authors) };
+  });
+  return result;
+}
+
+function getAuthor(book, authors) {
+  const author = authors.find((author) => author.id === book.authorId);
+  return author;
+}
 
 module.exports = {
   findAccountById,
--- public/src/books.js
+++ public/src/books.js
@@ -1,8 +1,28 @@
-function findAuthorById(authors, id) {}
+function findAuthorById(authors, id) {
+  let auth = authors.find((author) => author.id === id); //determines if the id is equal to an id in the authors database and returns that object if true
+  return auth;
+}
 
-function findBookById(books, id) {}
+function findBookById(books, id) {
+  let bookId = books.find((book) => book.id === id); //determines if the id is equal to an id in the book database and returns if true
+  return bookId;
+}
 
-function partitionBooksByBorrowedStatus(books) {}
+function partitionBooksByBorrowedStatus(books) {
+  let returnedBooks = [];
+  let missingBooks = [];
+  let bookArray = [];
+  const stillOut = books.find((book) => {
+    if(!!book.borrows.returned){
+      missingBooks.push(book);
+    }else{
+      returnedBooks.push(book);
+    }
+  });
+  bookArray.push(missingBooks, returnedBooks);
+  console.log(bookArray);
+  return bookArray;
+}
 
 function getBorrowersForBook(book, accounts) {}
 
