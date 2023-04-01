e8bde9aa9c215fe14c8ef5398126f698c49744b1
Michael Wegner <wegnermichael14@gmail.com>
2023-03-31 20:54:13 -0500

    methods completed

--- public/src/books.js
+++ public/src/books.js
@@ -8,9 +8,31 @@ function findBookById(books, id) {
   return bookId;
 }
 
-function partitionBooksByBorrowedStatus(books) {}
+function _filterBorrowed(books, status) {
+  return books.filter(({ borrows }) => status === borrows[0].returned);
+}
 
-function getBorrowersForBook(book, accounts) {}
+function partitionBooksByBorrowedStatus(books) {
+  const returned = true;
+  const borrowed = !returned;
+  const borrowedBooks = _filterBorrowed(books, borrowed);
+  const returnedBooks = _filterBorrowed(books, returned);
+  return [[...borrowedBooks], [...returnedBooks]];
+}
+
+function _findElementById(elements, id) {
+  return elements.find((element) => element.id === id);
+}
+
+function getBorrowersForBook({ borrows }, accounts) {
+  const borrowers = [];
+  for (let record in borrows) {
+    const borrowId = borrows[record].id;
+    const matchingAccount = _findElementById(accounts, borrowId);
+    borrowers.push({ ...borrows[record], ...matchingAccount });
+  }
+  return borrowers.slice(0, 10);
+}
 
 module.exports = {
   findAuthorById,
--- public/src/home.js
+++ public/src/home.js
@@ -1,3 +1,22 @@
+function sorter(arr, slicer = 5) {
+  const newArr = [...arr];
+  return newArr
+    .sort(({ count: count1 }, { count: count2 }) => count2 - count1)
+    .slice(0, slicer);
+}
+
+function authorBorrow(books, id) {
+  return books.reduce((totalBorrows, { authorId, borrows }) => {
+    if (authorId === id) totalBorrows += arrayCount(borrows);
+    return totalBorrows;
+  }, 0);
+}
+
+function arrayCount(item) {
+  return item.length;
+}
+
+
 function getTotalBooksCount(books) {
   let amount = 0;
   const count = books.forEach((book) => amount++);
@@ -10,17 +29,44 @@ function getTotalAccountsCount(accounts) {
   return amount;
 }
 
-function getBooksBorrowedCount(books) {}
-
-function getMostCommonGenres(books) {
-  let genreCount = {};
-  const g = books.forEach((book) => genreCount.set(book.genre, genreCount.get(book.genre)++));
-  
+function getBooksBorrowedCount(books) {
+  return books.reduce((borrowCount, { borrows }) => {
+    const mostRecent = borrows[0];
+    if (!mostRecent.returned) borrowCount++;
+    return borrowCount;
+  }, 0);
 }
 
-function getMostPopularBooks(books) {}
+function getMostCommonGenres(books) {
+  return sorter(
+    books
+      .reduce((genres, book) => {
+        const matchingGenre = genres.find((genre) => genre.name === book.genre);
+        !matchingGenre
+          ? genres.push({ name: book.genre, count: 1 })
+          : matchingGenre.count++;
+        return genres;
+      }, [])
+  );
+}
 
-function getMostPopularAuthors(books, authors) {}
+function getMostPopularBooks(books) {
+  return sorter(
+    books.map(({ title, borrows }) => ({
+      name: title, 
+      count: arrayCount(borrows),
+    }))
+  );
+}
+
+function getMostPopularAuthors(books, authors) {
+  return sorter(
+    authors.map(({ name: { first, last }, id }) => ({
+      name: `${first} ${last}`, 
+      count: authorBorrow(books, id), 
+    }))
+  );
+}
 
 module.exports = {
   getTotalBooksCount,
